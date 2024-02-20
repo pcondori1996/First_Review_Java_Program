65ed305a47ba846de846a4a84cb11389c36e3bb0
Paulo <paulocondori1@gmail.com>
2024-02-20 12:36:11 -0500

    Did some datatypes review

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,29 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("The result of i is now: " + i);
-        }
+            ;
+
+    }
+
+        String example_1 = "10";
+        int firstNum = Integer.parseInt(example_1);
+
+        System.out.println("\n" + example_1);
+        System.out.println((2 * firstNum));
+
+
+        // Solving an equation
+
+        System.out.println("\n\nIn this section, I will be solving an equation ( (a+b)^2 = a^2 + 2ab + b^2 )");
+        int a = 25;
+        float b = 45.159f;
+        double value = a*a + 2*(a*b) + b*b;
+
+        System.out.println(value);
+        System.out.println((float)value); //I made this value into a float
+        System.out.println((int)value); //I made this value into a float
+
+        /* Instead of adding a (type), change it from the creation of the variable to save space */
+
     }
 }
